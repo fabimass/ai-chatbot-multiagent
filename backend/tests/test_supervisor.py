cc877882c3907867d27a64341e385391c33a5f86
fabimass <massotto.fabian@gmail.com>
2024-12-26 12:55:10 -0300

    supervisor is more inteligent and can pick relevant agents

--- backend/tests/test_supervisor.py
+++ backend/tests/test_supervisor.py
@@ -21,6 +21,19 @@ def supervisor():
         
         return Supervisor(agents)
 
+def test_get_relevant_agents(supervisor):
+    # Mock LLM response
+    supervisor.llm.return_value = "agent_1, agent_2"
+
+    agents = supervisor.get_relevant_agents({"question": "test_question", "history": []})
+    assert agents == { "relevant_agents": ["agent_1", "agent_2"] }
+
+    # Assert that the user question and the list of agents were used when choosing relevant agents
+    assert "test_question" in supervisor.llm.call_args[0][0].messages[1].content
+    assert "agent_1" in supervisor.llm.call_args[0][0].messages[0].content
+    assert "agent_2" in supervisor.llm.call_args[0][0].messages[0].content
+    assert "agent_3" in supervisor.llm.call_args[0][0].messages[0].content
+
 def test_generate_answer(supervisor):
     relevant_agents = ["agent_1", "agent_2", "agent_3"]   
 
@@ -29,15 +42,15 @@ def test_generate_answer(supervisor):
     result = supervisor.generate_answer(state)
     assert result == {"next": "agent_1"}
 
-    state = {"agents": {"agent_1": "response_1"}, "question": "test_question"}
+    state = {"agents": {"agent_1": "response_1"}, "question": "test_question", "relevant_agents": relevant_agents}
     result = supervisor.generate_answer(state)
     assert result == {"next": "agent_2"}
 
-    state = {"agents": {"agent_1": "response_1", "agent_2": "response_2"}, "question": "test_question"}
+    state = {"agents": {"agent_1": "response_1", "agent_2": "response_2"}, "question": "test_question", "relevant_agents": relevant_agents}
     result = supervisor.generate_answer(state)
     assert result == {"next": "agent_3"}
     
     # Test when all agents have responded
-    state = {"agents": {"agent_1": "response_1", "agent_2": "response_2", "agent_3": "response_3"}, "question": "test_question"}
+    state = {"agents": {"agent_1": "response_1", "agent_2": "response_2", "agent_3": "response_3"}, "question": "test_question", "relevant_agents": relevant_agents}
     result = supervisor.generate_answer(state)
     assert result == {"next": "FINISH"}
