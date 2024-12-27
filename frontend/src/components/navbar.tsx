e5c51f54b15e7f4cde4745ed7eeedbbfe31b52b2
fabimass <massotto.fabian@gmail.com>
2024-12-27 13:09:18 -0300

    adding agent status to the navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -4,28 +4,42 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@nextui-org/navbar";
-
-
-import { siteConfig } from "@/config/site";
-import {
-  GithubIcon,
-} from "@/components/icons";
+import { Spinner } from "@nextui-org/react";
+import { AgentIcon } from "@/components/icons";
 import { Logo } from "@/components/icons";
+import { useEffect, useState } from "react";
+import { color } from "framer-motion";
 
 export const Navbar = () => {
+  const [agents, setAgents] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Get the status of the agents
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/agents`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setAgents(result);
+        setLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
-          <Link
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
-          >
-            <Logo />
-          </Link>
+          <Logo />
         </NavbarBrand>
       </NavbarContent>
 
@@ -34,18 +48,38 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
+          {loading && <Spinner size="sm" color="primary" />}
+          {agents.map((agent) => (
+            <AgentIcon
+              name={agent["agent"]}
+              status={agent["healthy"]}
+              tooltip={true}
+            />
+          ))}
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
+        <NavbarMenuToggle />
       </NavbarContent>
 
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {agents.map((item) => (
+            <NavbarMenuItem
+              key={item["agent"]}
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                gap: "20px",
+              }}
+            >
+              <AgentIcon name={item["agent"]} status={item["healthy"]} />
+              <span>{item["agent"]}</span>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
     </NextUINavbar>
   );
 };
