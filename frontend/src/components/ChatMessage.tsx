a655a916b58b7a0c566d3d3c720f96306134fde6
fabimass <massotto.fabian@gmail.com>
2024-12-27 13:25:39 -0300

    better aligned thumbs up and down

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -62,38 +62,40 @@ export const ChatMessage = ({ text, sender, previous }: ChatMessageProps) => {
 
   return (
     <div className="my-5">
-      <MessageBox
-        position={sender === "human" ? "right" : "left"}
-        type="text"
-        title={sender}
-        text={text}
-        id=""
-        focus={false}
-        date={new Date()}
-        titleColor="rgba(12, 105, 176, 1)"
-        forwarded={false}
-        replyButton={false}
-        removeButton={false}
-        status="read"
-        notch={true}
-        retracted={false}
-      />
-      {sender === "bot" && showIcons && (
-        <div className="flex space-x-2 mt-2 mx-5">
-          <button
-            onClick={handleThumbsUp}
-            className={`text-lg ${liked === true ? "text-green-500" : "text-gray-500"} ${animating ? (liked === true ? "animate-ping" : "animate-fade") : ""} hover:text-green-300`}
-          >
-            <FaThumbsUp />
-          </button>
-          <button
-            onClick={handleThumbsDown}
-            className={`text-lg ${liked === false ? "text-red-500" : "text-gray-500"} ${animating ? (liked === false ? "animate-ping" : "animate-fade") : ""} hover:text-red-300`}
-          >
-            <FaThumbsDown />
-          </button>
-        </div>
-      )}
+      <div className={`relative ${sender === "bot" ? "inline-block" : ""}`}>
+        <MessageBox
+          position={sender === "human" ? "right" : "left"}
+          type="text"
+          title={sender}
+          text={text}
+          id=""
+          focus={false}
+          date={new Date()}
+          titleColor="rgba(12, 105, 176, 1)"
+          forwarded={false}
+          replyButton={false}
+          removeButton={false}
+          status="read"
+          notch={true}
+          retracted={false}
+        />
+        {sender === "bot" && showIcons && (
+          <div className="flex space-x-2 mt-2 absolute top-1 right-4">
+            <button
+              onClick={handleThumbsUp}
+              className={`text-lg ${liked === true ? "text-green-500" : "text-gray-500"} ${animating ? (liked === true ? "animate-ping" : "animate-fade") : ""} hover:text-green-300`}
+            >
+              <FaThumbsUp />
+            </button>
+            <button
+              onClick={handleThumbsDown}
+              className={`text-lg ${liked === false ? "text-red-500" : "text-gray-500"} ${animating ? (liked === false ? "animate-ping" : "animate-fade") : ""} hover:text-red-300`}
+            >
+              <FaThumbsDown />
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
